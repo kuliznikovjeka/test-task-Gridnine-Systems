36f6bf153e04cf50da62da48fb76eedf5b37f47e
Jeka <buterfly0013@mail.ru>
2024-06-10 17:32:34 +0300

    fix: fix bugs for deploy

--- src/api/services/fakeFetch.ts
+++ src/api/services/fakeFetch.ts
@@ -4,7 +4,7 @@ import { FlightResponse } from '../types';
 export function fakeFetch(): Promise<FlightResponse> {
   return new Promise<FlightResponse>(resolve => {
     setTimeout(() => {
-      resolve(flightsData as FlightResponse);
+      resolve(flightsData as unknown as FlightResponse);
     }, 1000);
   });
 }
